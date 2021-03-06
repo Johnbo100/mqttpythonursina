d5218c711cb2f486734b894df2cc767bdbc20483
Johnbo100 <johnkeartonjones@googlemail.com>
2021-03-06 09:28:27 +0700

    Add files via upload

--- mqttursina.py
+++ mqttursina.py
@@ -9,6 +9,9 @@ zval = ""
 xint = 0
 yint = 0
 zint = 0
+axint = 0
+ayint = 0
+azint = 0
 
 
 client.connect("127.0.0.1", 1883, 60)
@@ -21,6 +24,9 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("gyroX")
     client.subscribe("gyroY")
     client.subscribe("gyroZ")
+    client.subscribe("accelerationX")
+    client.subscribe("accelerationY")
+    client.subscribe("accelerationZ")
 client.on_connect = on_connect
 # The callback for when a PUBLISH message is received from the server.
 
@@ -28,14 +34,14 @@ app = Ursina()
 cube = Entity(model='cube', color=color.orange, scale=(2,2,2))   
 Text.size = 0.05
 Text.default_resolution = 1080 * Text.size
-info = Text(text="A powerful waterfall roaring on the mountains")
+info = Text(text="Waiting for response")
 info.x = -0.5
 info.y = 0.4
 info.background = True
 info.visible = True    
 
 def on_message(client, userdata, msg):
-    time.sleep(0.2)
+    time.sleep(0.01)
     msgsubstring = str(msg.payload)
     trimedstring = msgsubstring.replace('b', '')#stripping the b char from the string
     trimedstring1 = trimedstring.replace("'","") 
@@ -43,17 +49,28 @@ def on_message(client, userdata, msg):
     
 
     xval=info.text.split(" ")
-    print('xval is:' + xval[0])
-    print('yval is:' + xval[1])
-    print('zval is:' + xval[2])
+    #print('xval is:' + xval[0])
+    #print('yval is:' + xval[1])
+    #print('zval is:' + xval[2])
+    #print('axval is:' + xval[3])
+    #print('ayval is:' + xval[4])
+    #print('azval is:' + xval[5])
     
     xint = int(float(xval[0]))
     yint = int(float(xval[1]))
     zint = int(float(xval[2]))
-    print('the value of cubey is :' + str(xint * 10))
+    axint = int(float(xval[3]))
+    ayint = int(float(xval[4]))
+    azint = int(float(xval[5]))
+    #print('the value of cubey is :' + str(xint * 10))
     cube.rotation_y += yint *10
     cube.rotation_x += xint *10
     cube.rotation_z += zint *10
+    cube.x += (axint+1) /10
+    #cube.y += (ayint) /10
+    cube.z += (ayint) /10
+
+    #info.text=str(cube.rotation_x, cube.rotation_y,  cube.rotation_z)
 
 client.on_message = on_message
 client.loop_start()
